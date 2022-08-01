591d08c72bd5aaf7ac595bc753ba2b00901708e3
gunny6026 <gunny6026@naver.com>
2022-08-01 16:00:33 +0900

    feat : js call stack

---
+++ Nodejs-master/README.md
@@ -0,0 +1,7 @@
+# Cookie
+
+
+HTTP 쿠키는 서버가 사용자의 웹 브라우저에 전송하는 작은 데이터 조각입니다.
+
+[홈페이지](https://developer.mozilla.org/ko/docs/Web/HTTP/Cookies)
+
---
+++ js_guide/callstack/index.js
@@ -0,0 +1,17 @@
+function three() {
+  console.log("i love js");
+}
+
+function two() {
+  three();
+}
+
+function one() {
+  two();
+}
+
+function zero() {
+  one();
+}
+
+zero();
