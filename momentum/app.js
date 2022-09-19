d2c6d1a205abd93a2563ca5abadf783fef7b2d75
gunny6026 <gunny6026@naver.com>
2022-09-19 12:48:47 +0900

    feat : 로컬 스토리지에서 유저 정보 가져오기

--- momentum/app.js
+++ momentum/app.js
@@ -1,3 +1,34 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const link = document.querySelector("a");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
+});
+
+link.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(event);
+});
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+const paintGreeting = (username) => {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+};
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+  paintGreeting(savedUsername);
+}
