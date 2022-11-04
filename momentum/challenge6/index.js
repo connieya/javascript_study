bb7b2bd08d8b36076b27b7184fbd87ce4accf436
gunny6026 <gunny6026@naver.com>
2022-11-04 22:51:32 +0900

    feat : setInterval & new Date() & padStart 로 시계

--- momentum/challenge6/index.js
+++ momentum/challenge6/index.js
@@ -7,3 +7,16 @@ for (let i = 0; i <= 6; i++) {
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 document.body.style.backgroundImage = `url(./img/${chosenImage})`;
 document.body.style.backgroundSize = "100% 100%";
+
+const clock = document.querySelector("#clock");
+setInterval(() => {
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+  clock.innerHTML = `${year}년 ${month}월 ${day}일 ${hour}:${minute}:${second}`;
+}, 1000);
