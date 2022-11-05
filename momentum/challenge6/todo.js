6205a4dd9ffec95319a631d0928b0d7ed836018c
gunny6026 <gunny6026@naver.com>
2022-11-05 15:50:33 +0900

    feat : 로컬스토리지를 이용한 Todo 삭제 구현

--- momentum/challenge6/todo.js
+++ momentum/challenge6/todo.js
@@ -11,19 +11,22 @@ const saveTodos = () => {
 
 const paintTodo = (todo) => {
   const li = document.createElement("li");
+  li.id = todo.id;
   li.style.color = " rgb(250, 248, 227)";
   li.style.fontWeight = "bold";
   const span = document.createElement("span");
   span.innerText = todo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
-  button.addEventListener("click", (event) => {
-    console.dir(event.target.parentElement);
-    console.log(event.target);
-  });
   li.appendChild(span);
   li.appendChild(button);
   ul.appendChild(li);
+  button.addEventListener("click", (event) => {
+    let target = event.target.parentElement;
+    target.remove();
+    todos = todos.filter((todo) => todo.id != parseInt(target.id));
+    saveTodos();
+  });
 };
 
 todoForm.addEventListener("submit", (event) => {
@@ -40,12 +43,9 @@ todoForm.addEventListener("submit", (event) => {
 });
 
 const todoList = localStorage.getItem(TODOS_KEY);
-console.log(todoList);
-console.log(typeof todoList);
 
 if (todoList) {
   const parseedTodo = JSON.parse(todoList);
-  console.log(parseedTodo);
   todos = parseedTodo;
   parseedTodo.forEach(paintTodo);
 }
